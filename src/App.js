4a60640d609185373757cf2b340af527708fba8a
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-01-25 01:29:57 +0530

    m

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import logo from './images/S.png';
 import banner from './images/Untitled design.png';
+import star from './images/Untitled_design-removebg-preview.png'
 import { Link } from 'react-router-dom';
 
 const App = () => {
@@ -13,29 +14,46 @@ const App = () => {
       window.open(whatsappLink, '_blank');
     };
   return (
-    <Fragment>
-      <div className='main-div'>
-        <div className='header'>
-          <header>
-            <div className='logo'>
-              <img src={logo} alt='Saz PG' />
-            </div>
-            <div className='header-btns'>
-              <button className='header-btn'>About us</button>
-              <button className='header-btn'>Details</button>
-              <button className='header-btn'>Book now</button>
-              <button className='contact-us-btn-spcbtn' onClick={openWhatsAppChat}>Contact us</button>
-            </div>
-          </header>
+<Fragment>
+  <div className='main-div'>
+    <div className='header'>
+      <header>
+        <div className='logo'>
+          <img src={logo} alt='Saz PG' />
         </div>
-      </div>
-      <div className='banner'>
-        <div className='banner-txt'>
-        <h2>Book your room<br/> now!</h2>
-        <button>Book Now</button>
+        <div className='header-btns'>
+          <button className='header-btn'>About us</button>
+          <button className='header-btn'>Details</button>
+          <button className='header-btn' onClick={openWhatsAppChat}>Book now</button>
+          <button className='contact-us-btn-spcbtn' onClick={openWhatsAppChat}>Contact us</button>
         </div>
-      </div>
-    </Fragment>
+      </header>
+    </div>
+  </div>
+  <div className='banner'>
+    <div className='banner-txt'>
+      <h2>Book your room<br/> now!</h2>
+      <button onClick={openWhatsAppChat}>Book Now</button>
+    </div>
+  </div>
+  <div className='some-breif-description'>
+    <section className='box-sbd'>
+        <h3>500+<br/>
+        Stays
+        </h3>
+    </section>
+    <section className='box-sbd-spc'>
+        <h3>4.6 <img src={star} style={{width: '20px'}}/><br/>
+        Rating
+        </h3>
+    </section>
+    <section className='box-sbd'>
+        <h3>15+<br/>
+            Years old
+        </h3>
+    </section>
+   </div>
+</Fragment>
   );
 };
 
