eeecf88d12dc8b6e3d43f1d4bab4ef10283063dd
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-01-25 11:29:45 +0530

    m

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
         Stays
         </h3>
     </section>
-    <section className='box-sbd-spc'>
+    <section className='box-sbd'>
         <h3>4.6 <img src={star} style={{width: '20px'}}/><br/>
         Rating
         </h3>
@@ -55,6 +55,17 @@ const App = () => {
    </div>
    <div className='aboutUs'>
         <section className='abtusimg'></section>
+        <section className='abtus-text'>
+          <h2>About us</h2>
+          <p>
+Welcome to Saz PG, an exclusive
+ space crafted for men seeking camaraderie and
+  comfort. Enjoy savory meals, 2-4 sharing rooms
+   with attached bathrooms, a washing machine,
+    speedy WiFi, and more. It's not just
+     accommodation; it's brotherhood. Welcome 
+     to your home away from home!</p>
+        </section>
    </div>
    </div>
 </Fragment>
